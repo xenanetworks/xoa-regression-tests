b4bd27b78650fd65ec2ca040f3187918ab9a4a4c
fpfeng <minifen9@gmail.com>
2023-02-10 16:55:53 +0800

    refactor: DRY

--- tasks.py
+++ tasks.py
@@ -5,22 +5,37 @@ from invoke import task
 COLOR_OUTPUT = platform == 'linux'
 
 
+def make_cmd(env: str) -> str:
+    return f"tox -e {env} -r"
+
 @task
 def git_china(c):
     """execute testing with git-requires.txt and china tester"""
-    c.run("tox -e git-china-tester", pty=COLOR_OUTPUT)
+    c.run(
+        make_cmd("git-china-tester"),
+        pty=COLOR_OUTPUT,
+    )
 
 @task
 def git_demo(c):
     """execute testing with git-requires.txt and demo tester"""
-    c.run("tox -e git-demo-tester", pty=COLOR_OUTPUT)
+    c.run(
+        make_cmd("git-demo-tester"),
+        pty=COLOR_OUTPUT,
+    )
 
 @task
 def pip_china(c):
     """execute testing with pip-requires.txt and china tester"""
-    c.run("tox -e pip-china-tester", pty=COLOR_OUTPUT)
+    c.run(
+        make_cmd("pip-china-tester"),
+        pty=COLOR_OUTPUT,
+    )
 
 @task
 def pip_demo(c):
     """execute testing with pip-requires.txt and demo tester"""
-    c.run("tox -e pip-demo-tester", pty=COLOR_OUTPUT)
+    c.run(
+        make_cmd("pip-demo-tester"),
+        pty=COLOR_OUTPUT,
+    )
