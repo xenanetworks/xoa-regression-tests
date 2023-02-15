cb38b8c730c6dc59dcee2b1f7d722db3ad0d85b1
fpfeng <minifen9@gmail.com>
2023-02-15 03:06:50 -0500

    feat: new env task

--- tasks.py
+++ tasks.py
@@ -9,33 +9,17 @@ def make_cmd(env: str) -> str:
     return f"tox -e {env} -r"
 
 @task
-def git_china(c):
-    """execute testing with git-requires.txt and china tester"""
-    c.run(
-        make_cmd("git-china-tester"),
-        pty=COLOR_OUTPUT,
-    )
-
-@task
-def git_demo(c):
-    """execute testing with git-requires.txt and demo tester"""
-    c.run(
-        make_cmd("git-demo-tester"),
-        pty=COLOR_OUTPUT,
-    )
-
-@task
-def pip_china(c):
-    """execute testing with pip-requires.txt and china tester"""
-    c.run(
-        make_cmd("pip-china-tester"),
-        pty=COLOR_OUTPUT,
-    )
-
-@task
-def pip_demo(c):
+def exec_test_suite_specific_requirement_denmark_tester(c):
     """execute testing with pip-requires.txt and demo tester"""
     c.run(
-        make_cmd("pip-demo-tester"),
+        make_cmd("exec-test-suite-specific-requirement-denmark-tester"),
+        pty=COLOR_OUTPUT,
+    )
+@task
+
+def exec_test_suite_specific_requirement_china_tester(c):
+    """execute testing with pip-requires.txt and demo tester"""
+    c.run(
+        make_cmd("exec-test-suite-specific-requirement-china-tester"),
         pty=COLOR_OUTPUT,
     )
